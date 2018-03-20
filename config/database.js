bf2af8f53fb288f96b6a5ff68b66c710a26de69e
Ashish Pal <developerashishpal@gmail.com>
2018-03-20 23:57:46 +0530

    update in db connection

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports ={
-    "url": 'mongodb://localhost/filetracker'
+    //"url": 'mongodb://localhost/filetracker'
+    "url": process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
 }
